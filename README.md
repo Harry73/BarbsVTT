caf5560a36a05ea38fcb8e6d4bca0471530cead7
Ian Patel <ian.patel@yahoo.com>
2021-07-24 21:05:57 -0400

    beginning

---
+++ README.md
@@ -0,0 +1,3 @@
+# Barbs
+
+Barbs is a custom tabletop RPG made by a friend of mine. As this is a custom system, we've built our own support for it.
---
+++ barbs.js
@@ -0,0 +1,7 @@
+// --------------------------------------------
+//  Foundry VTT Initialization
+// --------------------------------------------
+
+Hooks.once("init", function() {
+  console.log(`Barbs | Initializing Barbs System`);
+});
